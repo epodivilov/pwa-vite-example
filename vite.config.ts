31397daee06d2fcf5013b607ece2fb673c1708e5
Evgeniy Podivilov <evgeniy.podivilov@gmail.com>
2025-04-01 15:17:00 +0100

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,44 @@
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: false,
+
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+
+      manifest: {
+        name: "pwa-esample",
+        short_name: "pwa-esample",
+        description: "pwa-esample",
+        theme_color: "#f3eff6",
+        background_color: "#f3eff6",
+      },
+
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
+
+      devOptions: {
+        enabled: false,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
+      },
+    }),
+  ],
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+  },
+});
