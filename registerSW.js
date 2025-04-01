e016bd232e6cb3122faf3b92a6143f445ab9c7a2
Evgeniy Podivilov <evgeniy.podivilov@gmail.com>
2025-04-01 22:09:50 +0100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/pwa-vite-example/sw.js', { scope: '/pwa-vite-example/' })})}
